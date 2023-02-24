e7404338e003f520c4d6acd860181d6befbdc3e4
pooria eshghi malayeri <100807060+PooriaEshghi@users.noreply.github.com>
2023-02-24 09:43:08 +0100

    fix consol.log

--- src/app/clips-list/clips-list.component.ts
+++ src/app/clips-list/clips-list.component.ts
@@ -6,38 +6,33 @@ import { DatePipe } from '@angular/common';
   selector: 'app-clips-list',
   templateUrl: './clips-list.component.html',
   styleUrls: ['./clips-list.component.css'],
-  providers: [DatePipe]
+  providers: [DatePipe],
 })
-export class ClipsListComponent implements OnInit,OnDestroy {
-  @Input() scrollable = true
-  constructor(public clipService: ClipService){
-    this.clipService.getClips()
+export class ClipsListComponent implements OnInit, OnDestroy {
+  @Input() scrollable = true;
+  constructor(public clipService: ClipService) {
+    this.clipService.getClips();
   }
-  
-  ngOnInit():void{
-    if(this.scrollable){
 
-      window.addEventListener('scroll', this.handleScroll)
+  ngOnInit(): void {
+    if (this.scrollable) {
+      window.addEventListener('scroll', this.handleScroll);
     }
   }
   ngOnDestroy(): void {
-    if(this.scrollable){
-
-      window.removeEventListener('scroll',this.handleScroll)
-
+    if (this.scrollable) {
+      window.removeEventListener('scroll', this.handleScroll);
     }
-    this.clipService.pageClips = []
+    this.clipService.pageClips = [];
   }
   handleScroll = () => {
-    const {scrollTop, offsetHeight} = document.documentElement
-    const {innerHeight} = window
-    
-    const bottomOfWindow = Math.round(scrollTop) + innerHeight === offsetHeight
-    
-    if(bottomOfWindow){
-      this.clipService.getClips()
-      console.log('scrolled')
-      
+    const { scrollTop, offsetHeight } = document.documentElement;
+    const { innerHeight } = window;
+
+    const bottomOfWindow = Math.round(scrollTop) + innerHeight === offsetHeight;
+
+    if (bottomOfWindow) {
+      this.clipService.getClips();
     }
-  }
+  };
 }
